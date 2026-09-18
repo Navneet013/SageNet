2a3cbfa8cc79948c8b2a062ba17625d43271760a
Navneet013 <imnavneet509@gmail.com>
2026-09-18 21:21:19 +0530

    Initial Spring Boot project setup

---
+++ SageNet/src/main/java/com/project/SageNet/SageNetApplication.java
@@ -0,0 +1,13 @@
+package com.project.SageNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SageNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SageNetApplication.class, args);
+	}
+
+}
---
+++ SageNet/src/test/java/com/project/SageNet/SageNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.SageNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SageNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
